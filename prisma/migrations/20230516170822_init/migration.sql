a09385ee4917c50615612acfc716f29bd3fc2790
Dario Turin <dturin1997@gmail.com>
2023-05-16 12:12:29 -0500

    [IMP] User Model

---
+++ prisma/migrations/20230516170822_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `lastname` VARCHAR(191) NOT NULL,
+    `dni` INTEGER NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `birthdate` DATE NOT NULL,
+    `isAdmin` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_dni_key`(`dni`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
